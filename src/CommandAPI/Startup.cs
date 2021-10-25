143be26cccc5c11906484f2628af061466f9fa3b
crmdeveloper98 <crmdeveloper98@gmail.com>
2021-10-25 16:25:13 +0100

    Added some comments

--- src/CommandAPI/Startup.cs
+++ src/CommandAPI/Startup.cs
@@ -41,6 +41,7 @@ namespace CommandAPI
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, CommandContext context)
         {
+            // Added for migration
             context.Database.Migrate();
 
             if (env.IsDevelopment())
